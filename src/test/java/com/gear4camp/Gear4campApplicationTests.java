6eb667a030a2243cbd7de1ae608396bc47b8cf36
jinyeonseok <jinyeonseok@gmail.com>
2025-02-19 00:08:49 +0900

    init

---
+++ src/test/java/com/gear4camp/Gear4campApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gear4camp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gear4campApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
