4949a9ffcc98b1a32f520b3d77b43d1c2963e76d
jinyeonseok <jinyeonseok@gmail.com>
2025-03-25 13:40:05 +0900

    - @ActiveProfiles 어노테이션 추가(해당 부분으로 인해 ci/cd 실패 유력)

--- src/test/java/com/gear4camp/DatabaseJdbcTemplateTest.java
+++ src/test/java/com/gear4camp/DatabaseJdbcTemplateTest.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class DatabaseJdbcTemplateTest {
 
