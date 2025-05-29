e90ca10c563b55556c4ee473245d7d7b5c761966
jinyeonseok <jinyeonseok@gmail.com>
2025-05-29 17:33:34 +0900

    test: 테스트 중 활성 프로파일 로그 출력 추가
    - @BeforeEach에서 active profile, datasource URL, mapper path 출력
    - 환경별 로딩 확인 용도

--- src/test/java/com/gear4camp/DatabaseConnectionTest.java
+++ src/test/java/com/gear4camp/DatabaseConnectionTest.java
@@ -1,8 +1,10 @@
 package com.gear4camp;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.ActiveProfiles;
 
 import javax.sql.DataSource;
